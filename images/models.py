5b11e811d2ac85189334e1c84e142bf6419d2efb
Daniel Davies <danieldavies127@gmail.com>
2015-11-26 13:54:44 +0000

    linting issues

--- images/models.py
+++ images/models.py
@@ -10,7 +10,6 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
 from sorl.thumbnail import get_thumbnail
-from string import ascii_lowercase, ascii_uppercase, digits
 from images.base62 import base62encode
 
 
