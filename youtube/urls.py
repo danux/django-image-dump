2f7126ad44d106a7b58f6ac0334bb7a46946c28b
Daniel Davies <danieldavies127@gmail.com>
2016-08-08 22:58:29 +0100

    Added hyphens to youtube URLs

--- youtube/urls.py
+++ youtube/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns(
     '',
     url(r'^save/$', login_required(YoutubeVideoCreateView.as_view()), name='youtube_video_create'),
     url(r'^list/$', login_required(YoutubeVideoListView.as_view()), name='youtube_video_list'),
-    url(r'^(?P<youtube_id>[_\d\w]+)/$', YoutubeVideoDetailView.as_view(), name='youtube_video_detail'),
+    url(r'^(?P<youtube_id>[-_\d\w]+)/$', YoutubeVideoDetailView.as_view(), name='youtube_video_detail'),
 )
