117d5d1fbe5971459fd61216f626d20ba62871d7
Daniel Davies <danieldavies127@gmail.com>
2019-09-30 23:00:16 +0100

    Settings

--- image_dump/celery.py
+++ image_dump/celery.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import os
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
 
 from celery import Celery
 from django.conf import settings  # noqa
